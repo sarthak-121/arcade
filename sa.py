f7e5de26c69ab266f42054c63be4486b715a3292
sarthak-121 <58941836+sarthak-121@users.noreply.github.com>
2020-03-28 16:28:03 +0530

    -> Code of the game and its attributes

---
+++ sa.py
@@ -0,0 +1,134 @@
+import pygame
+pygame.init()
+
+win = pygame.display.set_mode((500,500))
+
+walkRight = [pygame.image.load('R1.png'), pygame.image.load('R2.png'), pygame.image.load('R3.png'), pygame.image.load('R4.png'), pygame.image.load('R5.png'), pygame.image.load('R6.png'), pygame.image.load('R7.png'), pygame.image.load('R8.png'), pygame.image.load('R9.png')]
+walkLeft = [pygame.image.load('L1.png'), pygame.image.load('L2.png'), pygame.image.load('L3.png'), pygame.image.load('L4.png'), pygame.image.load('L5.png'), pygame.image.load('L6.png'), pygame.image.load('L7.png'), pygame.image.load('L8.png'), pygame.image.load('L9.png')]
+bg = pygame.image.load('bg.png')
+char = pygame.image.load('standing.png')
+
+pygame.display.set_caption('test')
+win.blit(bg, (0,0))               #used to put images on window
+pygame.display.update()
+
+clock = pygame.time.Clock()
+
+class player():
+	def __init__(self ,x, y, height, width):
+		self.x = x
+		self.y = y
+		self.height = height
+		self.width = width
+		self.vel = 5
+		self.isJump = False
+		self.jumpCount = 10
+		self.right = False
+		self.left = False
+		self.walkCount = 0
+		self.standing = True
+		
+	def redraw(self ,win):
+		if self.walkCount + 1 >= 27:
+			self.walkCount = 0
+		if not(self.standing):
+			if self.left:
+				win.blit(walkLeft[self.walkCount//3], (self.x,self.y))
+				self.walkCount += 1
+			elif self.right:
+				win.blit(walkRight[self.walkCount//3], (self.x,self.y))
+				self.walkCount += 1
+		else:
+			if self.right:
+				win.blit(walkRight[0], (self.x,self.y))
+			else:
+				win.blit(walkLeft[0], (self.x,self.y))
+				
+				
+class projectile():
+	def __init__(self, x, y, radius, color, facing):
+		self.x = x
+		self.y = y		
+		self.radius = radius
+		self.color = color
+		self.facing = facing
+		self.vel = 8 * facing
+		
+	def draw(self ,win):
+		pygame.draw.circle(win, self.color, (self.x,self.y), self.radius)
+	
+	
+		
+def redrawGameWin():
+	win.blit(bg, (0,0))
+	box.redraw(win)
+	
+	for bullet in bullets:
+		bullet.draw(win)
+		
+	pygame.display.update()
+
+
+box = player(0, 350, 64, 64)	
+bullets = []
+
+run = True
+while run :
+
+	clock.tick(27)
+
+	for event in pygame.event.get() :
+		if event.type == pygame.QUIT:
+			run = False
+			
+	for bullet in bullets:
+		if bullet.x >= 0  and bullet.x <500:
+			bullet.x += bullet.vel
+		else:
+			bullets.pop(bullets.index(bullet))
+		
+	keys = pygame.key.get_pressed()    #returns dictionary
+	
+	if keys[pygame.K_SPACE]:
+		if box.left:
+			facing = -1
+		else:
+			facing = 1
+		
+		if len(bullets) < 5:		
+			bullets.append(projectile(round(box.x + box.width//2), round(box.y + box.height//2), 6, (255,0,0), facing))
+		
+	
+	if keys[pygame.K_RIGHT] and box.x < 500 - box.width - box.vel:
+		box.x += box.vel
+		box.right = True
+		box.left = False
+		box.standing = False
+	elif keys[pygame.K_LEFT] and box.x > box.vel :
+		box.x -= box.vel	
+		box.right = False
+		box.left = True
+		box.standing = False
+	else:
+		box .standing = True
+		box.walkCount = 0
+		
+	if not(box.isJump) :
+		if keys[pygame.K_UP] :
+			box.isJump = True
+	else :
+		if box.jumpCount >= -10:
+			neg = 1
+			if box.jumpCount < 0 :
+				neg = -1
+			box.y -= (box.jumpCount ** 2) * 0.5 * neg
+			box.jumpCount -= 1
+		else:
+			box.isJump = False
+			box.jumpCount = 10
+		
+		
+	
+	redrawGameWin()
+			
+pygame.quit()
